7da09fe3cc4b95b73b81878dafcf807d80c4b29f
Unknown <damien.p.ointier@gmail.com>
2018-10-09 13:59:44 +0200

    security filter + app constants

---
+++ src/fr/eni/qcm/ihm/filter/SecurityFilter.java
@@ -0,0 +1,47 @@
+package fr.eni.qcm.ihm.filter;
+
+import java.fr.eni.qcm.bo.Utilisateur;
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+//import java.fr.eni.qcm.common.AppConstants;
+
+public class SecurityFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+
+        //Récupération de l'objet utilisateur connecté depuis la session :
+        HttpSession session = httpRequest.getSession();
+        Utilisateur utilisateur = (Utilisateur) session.getAttribute( "utilisateur" );
+        
+        if(utilisateur == null) {
+            httpResponse.sendRedirect(String.format("%s/login", httpRequest.getContextPath()));
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+       
+    }
+
+}
---
+++ src/java/fr/eni/qcm/common/AppConstants.java
@@ -0,0 +1,9 @@
+package java.fr.eni.qcm.common;
+
+public class AppConstants {
+	
+    public static final String HTTP_PARAM_BOOLEAN_VALUE_ON = "on";
+    public static final String HTTP_PARAM_BOOLEAN_VALUE_OFF = "off";
+    public static final String UTF8 = "UTF-8";
+    
+}
