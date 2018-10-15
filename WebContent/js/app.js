7d797963dd9392e74b07dc8604acf8b7b4ac4318
Unknown <nisan6@wp.pl>
2018-10-15 16:30:24 +0200

    fix sccssscscss s qs sqd  q

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,13 +1,13 @@
 $(document).ready(function (){
-	$("#question_num_1").css('display', 'block');
-	$('.form_num_1').css('display', 'block');
+	$("#question_num_1").css('display', 'contents');
+	$('.form_num_1').css('display', 'contents');
 	$("span:contains('1')").addClass('active');
 	$(".box_question span").click(function(){
 		var value = $(this).text();
 		$(".form_question").each(function(){
 			$(this).css('display', 'none');
 		});
-		$('.form_num_' + value).css('display', 'block');
+		$('.form_num_' + value).css('display', 'contents');
 		if (!$(this).hasClass("active")){
 			$(".box_question span").each(function(){
 				$(this).removeClass(' active');
