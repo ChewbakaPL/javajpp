fc636c42353e337cb97f8749a35f36cfbecbf85d
Unknown <damien.p.ointier@gmail.com>
2018-10-16 11:34:17 +0200

    handle processEpreuve

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -16,3 +16,63 @@ $(document).ready(function (){
 		}
 	});
 });
+
+function sendData(){
+	let url = 'processEpreuve';
+	let formData = $('#formEpreuve').serializeArray();
+	
+	//let realArray = [];
+	let realObject = {};
+	Object.entries(formData).forEach(([index, obj]) => {
+	  var inputName = obj.name;
+	  if(inputName.indexOf("questions") === 0){
+		  console.log(inputName);
+		  
+		  inputName = inputName.replace("questions", "");
+		  inputName = inputName.replace("][", ";");
+		  inputName = inputName.replace("[", "");
+		  inputName = inputName.replace("]", "");
+		  
+		  var strSplit = inputName.split(";");
+		  var idQuestion = strSplit[0];
+		  var idPropositionChecked = strSplit[1];
+		  
+		  if(realObject["question"+idQuestion]){
+			  realObject["question"+idQuestion] += ";"+idPropositionChecked;
+		  }else{
+			  realObject["question"+idQuestion] = idPropositionChecked;
+		  }
+	  }
+	});
+	
+	//realArray["nbQuestion"] = realArray.length;
+	realObject["nbQuestion"] = Object.keys(realObject).length
+	
+	
+	console.log("realObject:");
+	console.log(realObject);
+	
+	
+	//JSON FAIL
+	//let jsonString = JSON.stringify(formData);
+	//let jsonString = JSON.stringify(realArray);
+	//console.log("jsonString:");
+	//console.log(jsonString);
+	  
+	//let dataToSend = {json:jsonString},
+	  
+	  
+	let dataToSend = realObject;
+	
+	
+	$.ajax({
+	  type: "POST",
+	  url: url,
+	  data: dataToSend,
+	  success: function(){
+		  console.log("AJAX SUCESS");
+		  console.log(postData);
+	  }
+	});
+}
+
