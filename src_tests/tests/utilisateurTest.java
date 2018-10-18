89a353995d446e21c788b4b1b39c269de36e0337
Unknown <nisan6@wp.pl>
2018-10-18 15:58:37 +0200

    diapo + test xD

--- src_tests/tests/utilisateurTest.java
+++ src_tests/tests/utilisateurTest.java
@@ -103,7 +103,6 @@ public class utilisateurTest {
 		result = utilisateurDao.checkLogin(emailUserToto, passwordUserToto);
 		assertNotNull(result);
 		assertNotEquals("admin", result.getType());
-		
 	}
 
 }
